0b6d2c12ec43366d9361c4058e1a4ca782d73172
Sajid <sajidzaman.2002.3@gmail.com>
2025-05-01 21:18:22 +0600

    fix HomeBar

--- src/components/HomeBar.vue
+++ src/components/HomeBar.vue
@@ -5,9 +5,9 @@
 </script>
 
 <template>
-        <section class="p-5 lg:mt-8 lg:m-8">
-            <div class="flex flex-row bg-gray-100 p-2 rounded-2xl gap-4 items-center justify-start overflow-x-auto 
-            whitespace-nowrap no-scrollbar md:justify-center lg:justify-start xl:justify-center">
+        <section class="flex flex-row justify-center p-5 lg:mt-8 lg:m-8">
+            <div class="flex flex-row bg-gray-100 p-2 w-fit rounded-2xl gap-4 items-center justify-start overflow-x-auto 
+            whitespace-nowrap no-scrollbar">
                 <HomeBarButton v-for="buttonName in buttonNames" :key="buttonName.id" :buttonName=buttonName />
             </div>
         </section>
