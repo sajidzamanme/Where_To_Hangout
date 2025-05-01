709e2681a87b01b78cbe0aab9280b64ea846f7d7
Sajid <sajidzaman.2002.3@gmail.com>
2025-05-01 21:09:57 +0600

    update HomeBar and componentization of buttons

--- src/components/HomeBar.vue
+++ src/components/HomeBar.vue
@@ -1,35 +1,14 @@
 <script setup>
+    import HomeBarButton from './HomeBarButton.vue';
 
+    const buttonNames = ["Popular", "Favorites", "Cafes", "Resturants", "Food Court"]
 </script>
 
 <template>
         <section class="p-5 lg:mt-8 lg:m-8">
-            <div class="flex flex-row gap-4 items-center justify-start overflow-x-auto 
+            <div class="flex flex-row bg-gray-100 p-2 rounded-2xl gap-4 items-center justify-start overflow-x-auto 
             whitespace-nowrap no-scrollbar md:justify-center lg:justify-start xl:justify-center">
-                <button class="flex flex-row min-w-[120px] h-[50px] bg-gray-300 
-                rounded-2xl items-center justify-center select-none hover:bg-gray-200">
-                    <h2 class="text-[18px] font-bold">Popular</h2>
-                </button>
-
-                <button class="flex flex-row min-w-[120px] h-[50px] bg-gray-300 
-                rounded-2xl items-center justify-center hover:bg-gray-200">
-                    <h2 class="text-[18px] font-bold">Favorites</h2>
-                </button>
-    
-                <button class="flex flex-row min-w-[120px] h-[50px] bg-gray-300 
-                rounded-2xl items-center justify-center hover:bg-gray-200">
-                    <h2 class="text-[18px] font-bold">Cafes</h2>
-                </button>
-    
-                <button class="flex flex-row min-w-[130px] h-[50px] bg-gray-300 
-                rounded-2xl items-center justify-center hover:bg-gray-200">
-                    <h2 class="text-[18px] font-bold">Resturants</h2>
-                </button>
-
-                <button class="flex flex-row min-w-[130px] h-[50px] bg-gray-300 
-                rounded-2xl items-center justify-center hover:bg-gray-200">
-                    <h2 class="text-[18px] font-bold">Food Court</h2>
-                </button>
+                <HomeBarButton v-for="buttonName in buttonNames" :key="buttonName.id" :buttonName=buttonName />
             </div>
         </section>
 </template>
